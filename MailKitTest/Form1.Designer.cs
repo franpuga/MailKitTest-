0814fa87a3d3f2a75cec6fea843db36b8e15a33b
Francisco Javier Puga Castro <fjpuga@aldaba.es>
2019-01-15 12:28:28 +0100

    Commit Inicial

---
+++ MailKitTest/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace MailKitTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDestinatario = new System.Windows.Forms.TextBox();
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.lblDestinatario = new System.Windows.Forms.Label();
+            this.lblAsunto = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDestinatario
+            // 
+            this.txtDestinatario.Location = new System.Drawing.Point(140, 83);
+            this.txtDestinatario.Name = "txtDestinatario";
+            this.txtDestinatario.Size = new System.Drawing.Size(291, 20);
+            this.txtDestinatario.TabIndex = 0;
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Location = new System.Drawing.Point(140, 120);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(291, 20);
+            this.txtAsunto.TabIndex = 1;
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(68, 159);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(669, 150);
+            this.txtMensaje.TabIndex = 2;
+            // 
+            // lblDestinatario
+            // 
+            this.lblDestinatario.AutoSize = true;
+            this.lblDestinatario.Location = new System.Drawing.Point(65, 86);
+            this.lblDestinatario.Name = "lblDestinatario";
+            this.lblDestinatario.Size = new System.Drawing.Size(63, 13);
+            this.lblDestinatario.TabIndex = 3;
+            this.lblDestinatario.Text = "Destinatario";
+            // 
+            // lblAsunto
+            // 
+            this.lblAsunto.AutoSize = true;
+            this.lblAsunto.Location = new System.Drawing.Point(65, 123);
+            this.lblAsunto.Name = "lblAsunto";
+            this.lblAsunto.Size = new System.Drawing.Size(40, 13);
+            this.lblAsunto.TabIndex = 4;
+            this.lblAsunto.Text = "Asunto";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(661, 325);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblAsunto);
+            this.Controls.Add(this.lblDestinatario);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.txtDestinatario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDestinatario;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Label lblDestinatario;
+        private System.Windows.Forms.Label lblAsunto;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ MailKitTest/Form1.cs
@@ -0,0 +1,63 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailKitTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSend_ClickAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress
+                                        ("Notificación",
+                                         "noreply@pugui.com"
+                                         ));
+                mimeMessage.To.Add(new MailboxAddress
+                                         ("ToTitle",
+                                         txtDestinatario.Text
+                                         ));
+                mimeMessage.Subject = txtAsunto.Text; //Subject  
+                mimeMessage.Body = new TextPart("plain")
+                {
+                    Text = txtMensaje.Text
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+                    // Esto funciona teniendo habilitada la opcion de aplicaciones menos seguras en gmail, si no hay que conseguir un token OAuth 2.0
+                    client.Authenticate(
+                        "pugui10@gmail.com",
+                        "XXXXXXXXX"
+                        );
+                    await client.SendAsync(mimeMessage);
+                    MessageBox.Show("Email enviado correctamente.");
+                    await client.DisconnectAsync(true);
+                }
+                txtDestinatario.Text = string.Empty;
+                txtAsunto.Text = string.Empty;
+                txtMensaje.Text = string.Empty;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+        }
+    }
+}
